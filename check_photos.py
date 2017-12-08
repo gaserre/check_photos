44e9e92c9f6ab8de0c508fb24341d32089ac79e6
Glenn Serre <gaserre@spiresoftware.com>
2017-12-08 10:36:51 -0800

    Pylint clean

--- check_photos.py
+++ check_photos.py
@@ -1,26 +1,26 @@
+#!/usr/bin/env python3
+'''
+Check that photos, etc. have been uploaded successfully
+'''
 from __future__ import print_function
-import httplib2
 import os
+import argparse
+import httplib2
 
 from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/drive-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+SCOPES = ('https://www.googleapis.com/auth/drive.metadata.readonly '
+          'https://www.googleapis.com/auth/drive.photos.readonly')
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Drive API Python Quickstart'
 
-
-def get_credentials():
+def get_credentials(flags):
     """Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
@@ -48,25 +48,45 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def main():
-    """Shows basic usage of the Google Drive API.
 
-    Creates a Google Drive API service object and outputs the names and IDs
-    for up to 10 files.
+class CheckPhotos():
+    '''
+    Check that media files (photos for now) were uploaded to google drive/photos,
+    verifying with the md5Checksum.
+    '''
+
+    def __init__(self, files):
+        self._files = files
+
+    def check(self, fname, checksum):
+        ''' check one file or tree '''
+        query = "name contains 'PIC00028.jpg'"
+        fields = "nextPageToken, files(id, name, md5Checksum, parents)"
+        entries = self._files.list(corpus='user',
+                                   spaces='photos,drive',
+                                   q=query,
+                                   fields=fields).execute()
+        files = entries.get('files', [])
+        for f in files:
+            print(f, checksum, fname)
+
+def main():
     """
-    credentials = get_credentials()
+    Check that pictures have been successfully uploaded to Google Photos or Drive.
+
+    Ensures that the file has been uploaded to Google photos or drive by comparing the md5sum to
+    that of all the files in drive and photos.
+    """
+    parser = argparse.ArgumentParser(parents=[tools.argparser])
+    parser.add_argument('dir_or_file', nargs='+', help='directory or file to check')
+    flags = parser.parse_args()
+
+    credentials = get_credentials(flags)
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('drive', 'v3', http=http)
+    checker = CheckPhotos(service.files())
 
-    results = service.files().list(
-        pageSize=10,fields="nextPageToken, files(id, name)").execute()
-    items = results.get('files', [])
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print('{0} ({1})'.format(item['name'], item['id']))
+    checker.check("PIC00028.jpg", "783763de6b07bb8d210a078d77016b7e")
 
 if __name__ == '__main__':
     main()
